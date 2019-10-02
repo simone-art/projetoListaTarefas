010bcfc5e5ab8bfe581e050ff6a623306d0aba17
simone-art <simoneperiodista3@gmail.com>
2019-10-02 13:54:46 -0300

    fizemos o javascript funcionar sem nenhum erro, mais falta botoes checar e remover

--- js/script.js
+++ js/script.js
@@ -1,41 +1,39 @@
 const containerPrincipal = document.getElementById("container");
 const form = document.getElementById("form");
 
-
-
-// Adicionar texto no campo adicioneTarefas 
 const adicioneTexto = document.getElementById("adicioneTarefa");
-
-// adicionar click no botao ADD 
 const botaoRoxo = document.getElementById("btn-roxa");
 const tabela = document.getElementById("Tabela");
+
 botaoRoxo.addEventListener("click", function(evento){
     evento.preventDefault();
-    if (adicioneTexto.value.trim()== ""){
-        let valorMensagem = text.value;
-        adicioneTexto.getAttribute("placeholder","Digite tarefa!");
-    } else
+    if (adicioneTexto.value.trim() == ""){
+        adicioneTexto.getAttribute("placeholder","Por favor, digite a sua tarefa:");
+        
+    } else {
         const tabelaFin = document.createElement("li");
-    tabelaFin.textContent = adicioneTexto.value;
-    console.log ("clickou");
-    tabela.appendChild(tabelaFin);
-    form.reset();
+        tabelaFin.classList.add("tarefas");
+        tabelaFin.textContent = adicioneTexto.value;
+        tabela.appendChild(tabelaFin);
+        form.reset();
+        
+        let buttonFin = document.createElement("span");
+        buttonFin.textContent = "X";
+        tabelaFin.appendChild(buttonFin);
+        
+        buttonFin.addEventListener("click", function (){
+            if (buttonFin.parentNode) {
+                tabelaFin.appendChild(buttonFin);
+                tabela.removeChild(tabelaFin);
+            }
+        })
+        tabelaFin.addEventListener("click", function(){
+            console.log(tabelaFin.classList)
+            if (tabelaFin.classList.contains("tarefas-completo")){
+                tabelaFin.classList.remove("tarefas-completo");
+            }else{
+                tabelaFin.classList.add("tarefas-completo");
+                }
+        })
     }
-
-//     if (document.getElementById ("btn-roxa").innerHTML = "desaparece input") {}
-    
 })
-
-// Aparecer o texto no campo Tarefa 1 (criar tabela)
-
-//Dar check na tarefa e tambén excluí-las
-
-// Validar campo de texto para nao incluir texto vacío
-
-
-// Limpar o campo de texto depoís que incluir texto na lista
-
-
-
-
-
